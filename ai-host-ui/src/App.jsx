429c5bdcb9b4eb4e25737094004fcc5e84f573e9
Vaughan Chase <vaughanc2k@gmail.com>
2025-09-23 21:59:57 -0700

    url

--- ai-host-ui/src/App.jsx
+++ ai-host-ui/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect, useRef } from 'react'
 import './App.css'
 
-const API_BASE_URL = import.meta.env.VITE_API_URL
-//const API_BASE_URL = 'http://localhost:5000'// default to localhost if env var is not set
+//const API_BASE_URL = import.meta.env.VITE_API_URL
+const API_BASE_URL = 'http://localhost:5000'// default to localhost if env var is not set
 function App() {
   const [threadId, setThreadId] = useState(null)
   const [threadInfo, setThreadInfo] = useState(null)
